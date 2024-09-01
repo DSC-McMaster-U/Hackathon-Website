341c081f9cfecf170d2b9d6c5245eba2c1ba6394
Aidan Froggatt <aidanfr@live.ca>
2024-09-01 17:29:50 -0400

    basic website structure and reference bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,76 @@
+import Page from "../components/Page";
+import RefBar from "../components/RefBar";
+
+const Hero = () => {
+	return (
+		<div
+			id="hero"
+			className="flex flex-col justify-center items-center h-screen"
+		>
+			<h1 className="text-4xl font-bold">GDSC</h1>
+			<p className="text-lg">Google Developer Student Clubs</p>
+		</div>
+	);
+};
+
+const About = () => {
+	return (
+		<div
+			id="about"
+			className="flex flex-col justify-center items-center h-screen"
+		>
+			<h1 className="text-4xl font-bold">About</h1>
+			<p className="text-lg">Learn about us</p>
+		</div>
+	);
+};
+
+const Sponsors = () => {
+	return (
+		<div
+			id="sponsors"
+			className="flex flex-col justify-center items-center h-screen"
+		>
+			<h1 className="text-4xl font-bold">Sponsors</h1>
+			<p className="text-lg">Our sponsors</p>
+		</div>
+	);
+};
+
+const Events = () => {
+	return (
+		<div
+			id="events"
+			className="flex flex-col justify-center items-center h-screen"
+		>
+			<h1 className="text-4xl font-bold">Events</h1>
+			<p className="text-lg">Past events</p>
+		</div>
+	);
+};
+
+const FAQ = () => {
+	return (
+		<div
+			id="faq"
+			className="flex flex-col justify-center items-center h-screen"
+		>
+			<h1 className="text-4xl font-bold">FAQ</h1>
+			<p className="text-lg">Frequently asked questions</p>
+		</div>
+	);
+};
+
 const Home = () => {
 	return (
-		<div>
-			<h1>Home</h1>
-		</div>
+		<>
+			<RefBar />
+			<Hero />
+			<About />
+			<Sponsors />
+			<Events />
+			<FAQ />
+		</>
 	);
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				googleBlue: "#174EA6",
+				googleRed: "#A50E0E",
+				googleOrange: "#E37400",
+				googleGreen: "#0D652D",
+				googleMediumBlue: "#4285F4",
+				googleMediumRed: "#EA4335",
+				googleYellow: "#FBBC04",
+				googleMediumGreen: "#34A853",
+				googleLightBlue: "#D2E3FC",
+				googleLightRed: "#FAD2CF",
+				googleLightYellow: "#FEEFC3",
+				googleLightGreen: "#CEEAD6",
+				googleLightGrey: "#F1F3F4",
+				googleGrey: "#9AA0A6",
+				googleBlack: "#202124",
+			},
+		},
 	},
 	plugins: [],
 };
