3f79b218ebbcef84540c2c075ff3a3c6304530c2
matthewferr <ferrem15@mcmaster.ca>
2024-09-03 16:42:24 -0400

    Update socialMedia.ts
    Fixed linkedin link

--- src/constants/socialMedia.ts
+++ src/constants/socialMedia.ts
@@ -11,7 +11,7 @@ export const socialMedia = [
 	},
 	{
 		name: "LinkedIn",
-		url: "https://www.instagram.com/gdscmcmasteru/",
+		url: "https://www.linkedin.com/company/gdscmcmasteru/",
 		icon: "SiLinkedin",
 	},
 ];
